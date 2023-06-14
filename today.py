99d0ecac16d4bce05ead79a2ce5f22affe8c6576
Kevin Mora <sci.kevinmora@gmail.com>
2023-06-14 15:44:42 -0700

    Update today.py

--- today.py
+++ today.py
@@ -38,7 +38,7 @@ def converttuple(tup):
 def readmeoverwrite():
     with open("README.md", "r") as file:
         data = file.readlines()
-        line4 = ('     -` .s-            -s. `-             Uptime: "', dailyreadme(), "\n")
+        line4 = (' Uptime: "', dailyreadme(), "\n")
     tup2str = converttuple(line4)
     data[4] = tup2str
 
